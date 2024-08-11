e18d7ffbaf39283052fdd748b37ecb959a0c35dd
Syafi Islam <91824733+SyafiIslam@users.noreply.github.com>
2024-08-11 18:42:53 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -62,7 +62,4 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation (libs.androidx.hilt.navigation.compose)
-
-    //LeakCanary
-    debugImplementation (libs.leakcanary.android)
 }
